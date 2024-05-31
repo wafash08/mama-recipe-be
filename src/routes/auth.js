1a33dd19e655ba2dffffc2e432826afa39f3e03c
muhammad risano <muhammadrisano@gmail.com>
2024-05-31 13:27:07 +0700

    add logout

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const { login, register } = require('../controllers/auth')
+const { login, register, logout } = require('../controllers/auth')
 const route = express.Router()
 
 route
     .post('/login', login)
     .post('/register', register)
+    .get('/logout', logout)
+
 
 module.exports = route
